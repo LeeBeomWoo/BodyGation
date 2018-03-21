5dd953525ac4742249f453c4d16e670dc0ff9b8b
LeeBeomWoo <vvaeru6@gmail.com>
2018-03-21 17:49:02 +0900

    youtube base

--- app/src/main/java/bodygate/bcns/bodygation/navigationitem/FollowFragment.kt
+++ app/src/main/java/bodygate/bcns/bodygation/navigationitem/FollowFragment.kt
@@ -36,7 +36,7 @@ class FollowFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_follow, container, false)
+        return inflater.inflate(R.layout.select_view, container, false)
     }
 
     // TODO: Rename method, update argument and hook method into UI event
